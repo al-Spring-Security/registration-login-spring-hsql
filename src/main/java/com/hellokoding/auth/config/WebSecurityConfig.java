b8b053d9c2b87886893908ab64485c1e05d513e6
lwk <qwfys200@qq.com>
2017-06-28 17:16:15 +0800

    添加thymeleaf支持

--- src/main/java/com/hellokoding/auth/config/WebSecurityConfig.java
+++ src/main/java/com/hellokoding/auth/config/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/resources/**", "/registration").permitAll()
+                    .antMatchers("/css/**", "/js").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
